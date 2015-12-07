09a8518245fcfe8d4558e9f3a6f8ea612423c671
Adrian Smith <agnomen@gmail.com>
2015-12-07 17:56:10 -0500

    First pass porting Starfield to javascript.

--- src/Starfield.js
+++ src/Starfield.js
@@ -1,127 +1,110 @@
-﻿/* ---------------------------------------------------------------
- * Copyright © Adrian Smith.
- * Licensed under the MIT license. See license.txt at project root.
- * --------------------------------------------------------------- */
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Audio;
-using Microsoft.Xna.Framework.Content;
-using Microsoft.Xna.Framework.GamerServices;
-using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
-using Microsoft.Xna.Framework.Media;
-using Microsoft.Xna.Framework.Net;
-using Microsoft.Xna.Framework.Storage;
+﻿function Point(x, y) {
+    this.x = x;
+    this.y = y;
+}
 
-namespace RGG2010
-{
-    class Starfield
+function scalePoint(p, s) {
+    return new Point(p.x * s, p.y + s);
+}
+
+function addPoints(a, b) {
+    return new Point(a.x + b.x, a.y + b.y);
+}
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function Starfield(width, height, density) {
+    this.visuals = [];
+    this.images = [];
+    this.maxSize = new Point(0,0);
+    this.size = new Point(width, height);
+    
+    this.frequency = {
+        "Galaxy": 1,
+        "Nebula": 1,
+        "Nebula2": 1,
+        "StarBlueGiant": 15,
+        "StarRedGiant": 20,
+        "StarRedDwarf": 50,
+        "StarSmallBlue": 100,
+        "StarYellow": 1000,
+    }
+    this.toLoad = Object.keys(frequency).length;
+    for (var imageName in this.frequency) {
+        addImage(imageName, this.frequency[imageName]);
+    }
+
+    function Visual(image, location, distance) {
+        this.location = location;
+        this.distance = distance;
+        this.image = image;
+
+        function tiledLocation(offset, tileSize)
+        {
+            var size = new Point(image.width, image.height)
+            var scaleFactor = 1.0 - this.distance;
+            var offsetLocation = addPoints(this.location, addPoints(scalePoint(offset, scaleFactor), size);
+            var distanceSize = scalePoint(tileSize, 1 /  scaleFactor);
+            var tiledX = offsetLocation.x % distanceSize.x;
+            if(tiledX < 0)
+            {
+                tiledX += distanceSize.x;
+            }
+            var tiledY = offsetLocation.y % distanceSize.y;
+            if(tiledY < 0)
+            {
+                tiledY += distanceSize.Y;
+            }
+            return new Point(tiledX - size.x, tiledY - size.y);
+        }
+    }
+  
+    function populate() {
+        var count = (int)(this.width * this.height * density);
+        var imageCount = this.images.length;
+        for (int i = 0; i < count; ++i)
+        {
+            var x = getRandomInt(0, width);
+            var y = getRandomInt(0, height);
+            var distance = Math.random()
+            var index = getRandomInt(0, imageCount);
+            var image = this.images[index];
+            this.visuals.push(new Visual(image, new Point(x, y), (distance * (imageCount - index)) / imageCount));
+        }
+    }
+
+    function addImage(name, frequency) {
+        var image = new Image();
+        var starfield = this;
+        image.onload = function() {
+            starfield.maxSize.x = Math.max(starfield.maxSize.x, image.width);
+            starfield.maxSize.y = Math.max(starfield.maxSize.y, image.height);
+            
+            for (var i = 0; i < frequency; ++i)
+            {
+                starfield.images.Add(image);
+            }
+            starfield.toLoad -= 1;
+            if(starfield.toLoad == 0) {
+                starfield.populate();
+            }
+        };
+        image.src = "/scrace/images/starfield/" + name + ".png";
+    }
+
+    function draw(canvas, offset, width, height)
     {
-        private class Visual
+        for (var i = 0; i < this.visuals.length; ++i)
         {
-            public Vector2 Location;
-            public float Distance;
-            public Texture2D Image;
-
-            public Visual(Texture2D image, Vector2 location, float distance)
-            {
-                Image = image;
-                Location = location;
-                Distance = distance;
-            }
-
-            internal Vector2 ScaleOffset(Vector2 offset)
-            {
-                return offset * (1 - Distance);
-            }
-
-            private Vector2 GetSize()
-            {
-                return new Vector2(Image.Width, Image.Height);
-            }
-
-            internal Vector2 TiledLocation(Vector2 offset, Vector2 tileSize)
-            {
-                Vector2 location = Location + offset * (1 - Distance) + GetSize();
-                Vector2 distanceSize = tileSize / (1 - Distance);
-                float tiledX = location.X % distanceSize.X;
-                if(tiledX < 0)
-                {
-                    tiledX += distanceSize.X;
-                }
-                float tiledY = location.Y % distanceSize.Y;
-                if(tiledY < 0)
-                {
-                    tiledY += distanceSize.Y;
-                }
-                return new Vector2(tiledX, tiledY) - GetSize();
-            }
-        }
-
-        private List<Visual> mVisuals = new List<Visual>();
-        private List<Texture2D> mImages = new List<Texture2D>();
-        private Vector2 mMaxImageSize = new Vector2(0, 0);
-        private Vector2 mSize;
-
-        public Starfield()
-        {
-        }
-
-        public void Populate(int width, int height, float density)
-        {
-            mSize = new Vector2(width, height);
-            Random rand = new Random();
-            int size = width * height;
-            int count = (int)(size * density);
-            for (int i = 0; i < count; ++i)
-            {
-                int x = rand.Next(width);
-                int y = rand.Next(height);
-                float distance = (float)rand.NextDouble();
-                int index = rand.Next(mImages.Count);
-                Texture2D image = mImages[index];
-                mVisuals.Add(new Visual(image, new Vector2(x, y), distance * (mImages.Count - index) / (float)(mImages.Count)));
-            }
-        }
-
-        public void LoadImages(ContentManager content)
-        {
-            AddImage(content, "Starfield/Galaxy", 1);
-            AddImage(content, "Starfield/Nebula", 1);
-            AddImage(content, "Starfield/Nebula2", 1);
-            AddImage(content, "Starfield/StarBlueGiant", 15);
-            AddImage(content, "Starfield/StarRedGiant", 20);
-            AddImage(content, "Starfield/StarRedDwarf", 50);
-            AddImage(content, "Starfield/StarSmallBlue", 100);
-            AddImage(content, "Starfield/StarYellow", 1000);
-        }
-
-        private void AddImage(ContentManager content, string resource, int frequency)
-        {
-            Texture2D image = content.Load<Texture2D>(resource);
-            mMaxImageSize = new Vector2(Math.Max(mMaxImageSize.X, image.Width), Math.Max(mMaxImageSize.Y, image.Height));
-            for (int i = 0; i < frequency; ++i)
-            {
-                mImages.Add(image);
-            }
-        }
-
-        public void Draw(SpriteBatch batch, Vector2 offset, int width, int height)
-        {
-            batch.Begin();
-            foreach (Visual v in mVisuals)
-            {
-                Vector2 location = v.TiledLocation(offset, mSize);
-                if (location.X < -mMaxImageSize.X || width < location.X)
-                    continue;
-                if (location.Y < -mMaxImageSize.Y || height < location.Y)
-                    continue;
-                batch.Draw(v.Image, location, Color.White);
-            }
-            batch.End();
+            var v = visuals[i];
+            var location = v.TiledLocation(offset, this.size);
+            if (location.x < -this.maxSize.x || width < location.x)
+                continue;
+            if (location.Y < -this.maxSize.Y || height < location.y)
+                continue;
+            canvas.drawImage(v.image, location.x, location.y);
         }
     }
 }
