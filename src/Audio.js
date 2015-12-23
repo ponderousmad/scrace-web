9e18a6e4cff02865366093e2b42e1b2cbc70de4d
Adrian Smith <agnomen@gmail.com>
2015-12-23 17:47:53 -0500

    Lint and prototypify Audio

--- src/Audio.js
+++ src/Audio.js
@@ -3,52 +3,52 @@
 var gAudioContext = null;
 try {
     gAudioContext = new (window.AudioContext || window.webkitAudioContext)();
-} catch(error) {
+} catch (error) {
     console.log("Error initializing audio:");
     console.log(error);
 }
 
-var SoundEffect = function(resource) {
+var SoundEffect = function (resource) {
     this.resource = resource;
     this._source = null;
     this._buffer = null;
     
-    var self = this;
-    
     resource = rootURL + resource;
+    var self = this;
     
     if (gAudioContext != null) {
         var request = new XMLHttpRequest();
         request.open("GET", resource, true);
         request.responseType = "arraybuffer";
-        request.onload = function() {
+        request.onload = function () {
             var audioData = request.response;
             gAudioContext.decodeAudioData(audioData,
-                function(buffer) {
+                function (buffer) {
                     self._buffer = buffer;
                 },
-                function(e) {
-                    "Error with decoding audio data" + e.err
+                function (e) {
+                    console.log("Error with decoding audio data" + e.err);
                 }
             );
-        }
+        };
         request.send();
     }
+};
+
     
-    this.isLoaded = function() {
-        return gAudioContext == null || self._buffer != null;
+SoundEffect.prototype.isLoaded = function () {
+    return gAudioContext == null || this._buffer != null;
+};
+
+SoundEffect.prototype.play = function () {
+    if (gAudioContext == null || this._buffer == null) {
+        return;
     }
-    
-    this.play = function() {
-        if (gAudioContext == null || self._buffer == null) {
-            return;
-        }
-        if (self._source) {
-            self._source.disconnect(gAudioContext.destination);
-        }
-        self._source = gAudioContext.createBufferSource();
-        self._source.buffer = self._buffer;
-        self._source.connect(gAudioContext.destination);
-        self._source.start();
+    if (this._source) {
+        this._source.disconnect(gAudioContext.destination);
     }
+    this._source = gAudioContext.createBufferSource();
+    this._source.buffer = this._buffer;
+    this._source.connect(gAudioContext.destination);
+    this._source.start();
 };
