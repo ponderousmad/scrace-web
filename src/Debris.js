898b6cde2eeafcad6b3fc8f512d5ef9fd38f93b2
Adrian Smith <agnomen@gmail.com>
2015-12-24 19:08:09 -0500

    Delinting Debris

--- src/Debris.js
+++ src/Debris.js
@@ -1,3 +1,4 @@
+"use strict";
 
 var DebrisType = {
     SmallAsteroid : 0,
@@ -5,7 +6,7 @@ var DebrisType = {
     PlayerCockpit : 2,
     PlayerLeft : 3,
     PlayerRight : 4
-}
+};
 
 var DebrisNames = {
     "SmallAsteroid" : DebrisType.SmallAsteroid,
@@ -25,7 +26,7 @@ var debrisImages = [
 ];
 debrisBatch.commit();
 
-var Debris = function(type, location, velocity) {
+var Debris = function (type, location, velocity) {
     this.kMaxSpin = 0.01;
     this.kMaxPlanetDistance = 200;
     this.kMaxPlanetDistanceSq = this.kMaxPlanetDistance * this.kMaxPlanetDistance;
@@ -34,29 +35,26 @@ var Debris = function(type, location, velocity) {
     this.spin = Math.random() * this.kMaxSpin;
     this.location = location;
     this.startLocation = location.clone();
-    this.velocity = velocity || new Vector(0,0);
+    this.velocity = velocity || new Vector(0, 0);
     this.startVelocity = velocity;
     this.type = type;
     this.destroyed = false;
     this.image = debrisImages[type];
-}
+};
 
-Debris.prototype.setStartVelocity = function(velocity)
-{
+Debris.prototype.setStartVelocity = function (velocity) {
     this.startVelocity = velocity;
     this.velocity = velocity;
-}
+};
 
-Debris.prototype.reset = function()
-{
+Debris.prototype.reset = function () {
     this.location = this.startLocation.clone();
     this.velocity = this.startVelocity ? this.startVelocity.clone() : new Vector(0, 0);
     this.destroyed = false;
-}
+};
 
-Debris.prototype.store = function() {
-    if (this.isPlayerDebris())
-    {
+Debris.prototype.store = function () {
+    if (this.isPlayerDebris()) {
         // Don't store exploding player.
         return;
     }
@@ -74,57 +72,55 @@ Debris.prototype.store = function() {
         }
     }
     */
-}
+};
 
-Debris.prototype.isPlayerDebris = function() {
+Debris.prototype.isPlayerDebris = function () {
     return this.type === DebrisType.PlayerCockpit || this.type === DebrisType.PlayerLeft || this.type === DebrisType.PlayerRight;
-}
+};
 
-Debris.prototype.size = function() {
+Debris.prototype.size = function () {
     return this.image.width * 0.5;
-}
+};
 
-Debris.prototype.setLocation = function(vector) {
+Debris.prototype.setLocation = function (vector) {
     this.location = vector;
     this.startLocation = vector;
-}
+};
 
-Debris.prototype.contains = function(point) {
+Debris.prototype.contains = function (point) {
     return pointDistance(this.location, point) < this.size() * 1.25;
-}
+};
 
-Debris.prototype.setDebrisType = function(type) {
+Debris.prototype.setDebrisType = function (type) {
     this.type = type;
     this.image = debrisImages[type];
-}
+};
 
-Debris.prototype.draw = function(context, offset) {
+Debris.prototype.draw = function (context, offset) {
     if (this.destroyed) {
         return;
     }
-    var xLoc = this.location.x + offset.x;
-    var yLoc = this.location.y + offset.y;
-    var halfSize = this.size();
+    var xLoc = this.location.x + offset.x,
+        yLoc = this.location.y + offset.y,
+        halfSize = this.size();
     
-    context.save();        
+    context.save();
     context.translate(xLoc, yLoc);
     context.rotate(this.angle);
-    context.drawImage(this.image, -halfSize, -halfSize, this.image.width, this.image.height);        
+    context.drawImage(this.image, -halfSize, -halfSize, this.image.width, this.image.height);
     context.restore();
-}
+};
 
-Debris.prototype.update = function(elapsed, planets) {
-    if (this.destroyed)
-    {
+Debris.prototype.update = function (elapsed, planets) {
+    if (this.destroyed) {
         return;
     }
     this.location.addScaled(this.velocity, elapsed);
     this.angle = clampAngle(this.angle + this.spin);
 
-    for (var i = 0; i < planets.length; ++i)
-    {
+    for (var i = 0; i < planets.length; ++i) {
         var p = planets[i];
-        var accel = p.determineAcceleration(this.location, this.kMaxPlanetDistanceSq, elapsed)
+        var accel = p.determineAcceleration(this.location, this.kMaxPlanetDistanceSq, elapsed);
         if (accel) {
             if (accel === "crash") {
                 this.destroyed = true;
@@ -133,9 +129,8 @@ Debris.prototype.update = function(elapsed, planets) {
             }
         }
     }
-}
+};
 
-Debris.prototype.setSpin = function(spin)
-{
+Debris.prototype.setSpin = function (spin) {
     this.spin = spin;
-}
+};
